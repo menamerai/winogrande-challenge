cf5e6c710417374d1f2b55502942aa1aa2486a9d
menamerai <rphananh@gmail.com>
2023-11-30 00:49:07 -0500

    reformat chain-of-thought

--- mistral-evaluate.py
+++ mistral-evaluate.py
@@ -18,15 +18,15 @@ def infer(prompt: str, option1: str, option2: str, correct: str) -> Dict:
     winogrande_prompt = PromptTemplate.from_template(
         """
         John moved the couch from the garage to the backyard to create space. The _ is small. "garage" or "backyard"?
-        A: The question is asking about whether the garage or the backyard is small. The couch was moved from the garage to make space, so the garage is small. The answer is "garage".
+        A: The question is asking about what is small, the backyard or the garage. The couch was moved from the garage to make space, so the garage is small. The answer is "garage".
         The doctor diagnosed Justin with bipolar and Robert with anxiety. _ had terrible nerves recently. "Justin" or "Robert"?
-        A: The question is asking about who has terrible nerves. Robert was diagnosed with anxiety, which has symptoms of nervousness. The answer is "Robert".
+        A: The question is asking about who has terrible nerves, Justin or Robert. Robert was diagnosed with anxiety, which has symptoms of nervousness. The answer is "Robert".
         Dennis drew up a business proposal to present to Logan because _ wants his investment. "Dennis" or "Logan"?
-        A: The question is asking about who wants the investment. Dennis drew up the business proposal, so he wants the investment. The answer is "Dennis".
+        A: The question is asking about who wants the investment, Logan or Dennis. Dennis drew up the business proposal, so he wants the investment. The answer is "Dennis".
         Felicia unexpectedly made fried eggs for breakfast in the morning for Katrina and now _ owes a favor. "Felicia" or "Katrina"?
-        A: The question is asking about who owes a favor. Katrina received Felicia's breakfast, but hasn't done anything in return, so she owes a favor. The answer is "Katrina".
+        A: The question is asking about who owes a favor, Katrina or Felicia. Katrina received Felicia's breakfast, but hasn't done anything in return, so she owes a favor. The answer is "Katrina".
         My shampoo did not lather easily on my Afro hair because the _ is too dirty. "shampoo" or "hair"?
-        A: The question is asking about what is dirty. The shampoo did not lather easily because dirty hair is hard to apply shampoo on. The answer is "hair".
+        A: The question is asking about what is dirty, the hair or the shampoo. The shampoo did not lather easily because dirty hair is hard to apply shampoo on. The answer is "hair".
         {prompt} "{option1}" or "{option2}"?
         A: 
         """
@@ -59,7 +59,7 @@ def infer(prompt: str, option1: str, option2: str, correct: str) -> Dict:
 if __name__ == "__main__":
     # use threading to speed up inference
     answers = []
-    for idx in tqdm(range(10)):
+    for idx in tqdm(range(100)):
         # replace all "_" with "[BLANK]"
         prompt = val_dataset["sentence"][idx]
         option1 = val_dataset["option1"][idx]
