b2c500cc132ae01322dfc4f727d50bb04e274562
menamerai <rphananh@gmail.com>
2023-11-30 01:17:33 -0500

    reformat prompt to common sense reasoning

--- mistral-evaluate.py
+++ mistral-evaluate.py
@@ -6,27 +6,21 @@ from langchain.prompts import PromptTemplate
 from langchain.llms import Ollama
 from tqdm import tqdm
 
-dataset = load_dataset("winogrande", "winogrande_debiased")
-train_dataset = dataset["train"]
-val_dataset = dataset["validation"]
-test_dataset = dataset["test"]
-llm = Ollama(model="mistral", temperature=0.8, top_p=0.9, top_k=0)
-
 
 def infer(prompt: str, option1: str, option2: str, correct: str) -> Dict:
     # chain-of-thought prompt
     winogrande_prompt = PromptTemplate.from_template(
         """
         John moved the couch from the garage to the backyard to create space. The _ is small. "garage" or "backyard"?
-        A: The question is asking about what is small, the backyard or the garage. The couch was moved from the garage to make space, so the garage is small. The answer is "garage".
+        A: The question is asking about what is small, the backyard or the garage. Common sense says that space needed to be created in the garage because it was perceived as not big enough, or small. The answer is "garage".
         The doctor diagnosed Justin with bipolar and Robert with anxiety. _ had terrible nerves recently. "Justin" or "Robert"?
-        A: The question is asking about who has terrible nerves, Justin or Robert. Robert was diagnosed with anxiety, which has symptoms of nervousness. The answer is "Robert".
+        A: The question is asking about who has terrible nerves, Justin or Robert. Common sense says that people who has anxiety are nervous, so Robert who was diagnosed with anxiety is likely to have terrible nerves. The answer is "Robert".
         Dennis drew up a business proposal to present to Logan because _ wants his investment. "Dennis" or "Logan"?
-        A: The question is asking about who wants the investment, Logan or Dennis. Dennis drew up the business proposal, so he wants the investment. The answer is "Dennis".
+        A: The question is asking about who wants the investment, Logan or Dennis. Common sense says that the person who make the proposal - Dennis - wants something from the other person. The answer is "Dennis".
         Felicia unexpectedly made fried eggs for breakfast in the morning for Katrina and now _ owes a favor. "Felicia" or "Katrina"?
-        A: The question is asking about who owes a favor, Katrina or Felicia. Katrina received Felicia's breakfast, but hasn't done anything in return, so she owes a favor. The answer is "Katrina".
+        A: The question is asking about who owes a favor, Katrina or Felicia. Common sense says that the person who received something - Katrina - and hasn't done anything in return owes a favor. The answer is "Katrina".
         My shampoo did not lather easily on my Afro hair because the _ is too dirty. "shampoo" or "hair"?
-        A: The question is asking about what is dirty, the hair or the shampoo. The shampoo did not lather easily because dirty hair is hard to apply shampoo on. The answer is "hair".
+        A: The question is asking about what is dirty, the hair or the shampoo. Common sense says that dirty hair, especially Afro hair, is hard and stiff, so it can be hard to lather shampoo on. The answer is "hair".
         {prompt} "{option1}" or "{option2}"?
         A: 
         """
@@ -57,7 +51,14 @@ def infer(prompt: str, option1: str, option2: str, correct: str) -> Dict:
     return output_dict
 
 if __name__ == "__main__":
-    # use threading to speed up inference
+    model = "mistral"
+
+    dataset = load_dataset("winogrande", "winogrande_debiased")
+    train_dataset = dataset["train"]
+    val_dataset = dataset["validation"]
+    test_dataset = dataset["test"]
+    llm = Ollama(model=model, temperature=0.8, top_p=0.9, top_k=0)
+
     answers = []
     for idx in tqdm(range(100)):
         # replace all "_" with "[BLANK]"
@@ -75,5 +76,5 @@ if __name__ == "__main__":
 
     # save answers to csv with pandas
     output_df = pd.DataFrame.from_records(answers)
-    output_df.to_csv("./output/mistral-evaluate-{}.csv".format(len(answers)), index=False)
+    output_df.to_csv("./output/{}-evaluate-{}.csv".format(model, len(answers)), index=False)
         
